609db0b77dc1c9c6d9712dfdbde575649e40ca6a
BIL-LAB2PC00\Ogrenci <seyydinburhanedin.mtal38@gmail.com>
2022-10-28 12:17:25 +0300

    no message

--- app/src/main/java/com/example/unite_2ekrantasarm/MainActivity.java
+++ app/src/main/java/com/example/unite_2ekrantasarm/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.unite_2ekrantasarm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+
+        Button Btnuyg3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main );
+
+
+        Btnuyg3 = findViewById(R.id.Btnuyg3);
+        Btnuyg3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent veysel = new Intent(MainActivity.this, UygActivity.class);
+                startActivity(veysel);
+            }
+        });
+
     }
 }
