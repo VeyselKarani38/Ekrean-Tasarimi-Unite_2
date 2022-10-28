9ab01550200e0ebf4d6b344e3002592581aac201
BIL-LAB2PC00\Ogrenci <seyydinburhanedin.mtal38@gmail.com>
2022-10-28 09:42:18 +0300

    28.102022

---
+++ app/src/main/java/com/example/unite_2ekrantasarm/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.unite_2ekrantasarm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
